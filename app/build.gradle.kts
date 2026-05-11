df8d6484e49842ab77f2e35afc78e8f66d3ca7cd
jeniffer-leme <jenifferl.dossantos@gmail.com>
2026-05-11 11:10:25 -0300

    Mudança de String

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,15 @@ android {
 
 dependencies {
 
+    val camerax_version = "1.5.0-beta01"
+    implementation("androidx.camera:camera-core:${camerax_version}")
+    implementation("androidx.camera:camera-camera2:${camerax_version}")
+    implementation("androidx.camera:camera-lifecycle:${camerax_version}")
+    implementation("androidx.camera:camera-video:${camerax_version}")
+    implementation("androidx.camera:camera-view:${camerax_version}")
+    implementation("androidx.camera:camera-mlkit-vision:${camerax_version}")
+    implementation("androidx.camera:camera-extensions:${camerax_version}")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
